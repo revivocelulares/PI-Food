2134f9e8b4341cfe8db2d18bc7630417e37a841f
Diego Ferreira <revivocelulares@gmail.com>
2022-03-12 01:37:05 -0300

    Final Commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ import Landing from './componentes/inicio/landing';
 function App() {
   return (
     <Switch>
-      <Route exat path="/detalles/:id" component={Detalles} />
-      <Route exat path="/home" component={Home} />
-      <Route exat path="/crearReceta" component={Crear} />
+      <Route exact path="/detalles/:id" component={Detalles} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/crearReceta" component={Crear} />
       <Route path="/" component={Landing} />
     </Switch>
   );
